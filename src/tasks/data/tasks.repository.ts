840ad1c6bd53604d89281af3415e6661799cde26
JoaoCardoso00 <jvcq13@gmail.com>
2022-07-15 00:18:38 -0300

    still need to fix no repository error

--- src/tasks/data/tasks.repository.ts
+++ src/tasks/data/tasks.repository.ts
@@ -1,6 +1,8 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Task } from "./task.entity";
+import { EntityRepository, Repository } from 'typeorm';
+import { Task } from './task.entity';
+
+//TODO: fix error
+//! ERROR [ExceptionHandler] No repository for "TasksRepository" was found. Looks like this entity is not registered in current "default" connection?
 
 EntityRepository(Task);
-export class TasksRepository extends Repository<Task> {
-}
+export class TasksRepository extends Repository<Task> {}
