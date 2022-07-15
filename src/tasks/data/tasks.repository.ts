a11cd09e2da12afa8b68ef506dd9fe7199825192
JoaoCardoso00 <jvcq13@gmail.com>
2022-07-14 22:02:17 -0300

    changed customRepository decorator

--- src/tasks/data/tasks.repository.ts
+++ src/tasks/data/tasks.repository.ts
@@ -1,6 +1,7 @@
+import { CustomRepository } from "src/database/typeorm-ex.decorator";
 import { EntityRepository, Repository } from "typeorm";
 import { Task } from "./task.entity";
 
-@EntityRepository(Task)
+@CustomRepository(Task)
 export class TasksRepository extends Repository<Task> {
 }
